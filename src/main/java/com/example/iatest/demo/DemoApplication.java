f81cc2920c918fe5ec87f111a1c6825d5a102694
Sarah Young <sarah.young@salesforce.com>
2020-08-04 15:05:27 -0300

    WIP

--- src/main/java/com/example/iatest/demo/DemoApplication.java
+++ src/main/java/com/example/iatest/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
